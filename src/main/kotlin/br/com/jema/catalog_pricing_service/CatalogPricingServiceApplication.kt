f8f6020662b391330789a14c26e106af4b3bf865
Jessica Etiene <etiene.jm@gmail.com>
2026-03-08 13:55:39 -0400

    feat: Add dependency for JPA (no database configured yet)

--- src/main/kotlin/br/com/jema/catalog_pricing_service/CatalogPricingServiceApplication.kt
+++ src/main/kotlin/br/com/jema/catalog_pricing_service/CatalogPricingServiceApplication.kt
@@ -1,9 +1,16 @@
 package br.com.jema.catalog_pricing_service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude = [
+		DataSourceAutoConfiguration::class,
+		HibernateJpaAutoConfiguration::class
+	]
+)
 class CatalogPricingServiceApplication
 
 fun main(args: Array<String>) {
