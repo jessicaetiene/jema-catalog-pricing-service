25476567a53fce276c5a4cbfcc284372b90d0240
Jessica Etiene <etiene.jm@gmail.com>
2026-03-07 19:39:16 -0500

    feat: bootstrap spring boot kotlin project

---
+++ src/main/kotlin/br/com/jema/catalog_pricing_service/CatalogPricingServiceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jema.catalog_pricing_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatalogPricingServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatalogPricingServiceApplication>(*args)
+}
