b27f9d3be4f350890c5c152af0ec9d9896c945d7
Jessica Etiene <etiene.jm@gmail.com>
2026-03-29 18:55:13 -0400

    config(database): configure postgres datasource

--- src/main/kotlin/br/com/jema/catalog_pricing_service/CatalogPricingServiceApplication.kt
+++ src/main/kotlin/br/com/jema/catalog_pricing_service/CatalogPricingServiceApplication.kt
@@ -5,12 +5,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-	exclude = [
-		DataSourceAutoConfiguration::class,
-		HibernateJpaAutoConfiguration::class
-	]
-)
+@SpringBootApplication
 class CatalogPricingServiceApplication
 
 fun main(args: Array<String>) {
