b3b2031cf24d1fd69326bfa2fe24ded8cebd62ea
Jessica Etiene <etiene.jm@gmail.com>
2026-04-18 18:49:42 -0400

    task: create schema product table

--- src/main/resources/db/migration/V1__initial_product_schema.sql
+++ src/main/resources/db/migration/V1__initial_product_schema.sql
@@ -1,7 +1,9 @@
-CREATE TABLE product (
+CREATE TABLE tbl_product (
                          id SERIAL PRIMARY KEY,
                          name VARCHAR(255) NOT NULL,
                          description TEXT,
-                         base_price NUMERIC(10,2) NOT NULL,
-                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+                         price NUMERIC(10,2) NOT NULL,
+                         active BOOLEAN NOT NULL,
+                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP
 );
